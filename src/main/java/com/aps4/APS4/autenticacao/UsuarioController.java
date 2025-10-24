b01ea12d5701b931f9c1e05fed406966481d59e1
iran <iranilsonhonorato88@gmail.com>
2025-10-24 16:42:39 -0300

    feat: implementando classe Usuario

--- src/main/java/com/aps4/APS4/autenticacao/UsuarioController.java
+++ src/main/java/com/aps4/APS4/autenticacao/UsuarioController.java
@@ -1,4 +1,54 @@
 package com.aps4.APS4.autenticacao;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Collection;
+
+
+@RestController
+@RequestMapping("/usuarios")
 public class UsuarioController {
+
+
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @GetMapping
+    public Collection<Usuario> listarUsuarioController() {
+        return usuarioService.listarUsuarios();
+    }
+
+    @GetMapping("/{email}")
+    public ResponseEntity<Usuario> buscarUsuarioController(@PathVariable String email) {
+        Usuario usuario = usuarioService.buscarUsuario(email);
+        if (usuario == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Usuário não encontrado");
+        }
+        return ResponseEntity.ok(usuario);
+    }
+
+    @PostMapping
+    public ResponseEntity<Usuario> cadastrarUsuarioController(@RequestBody Usuario usuario) {
+        Usuario criado = usuarioService.cadastrarUsuario(usuario);
+        return ResponseEntity.status(HttpStatus.CREATED).body(criado);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> loginController(@RequestBody Usuario usuario) {
+        String token = usuarioService.login(usuario);
+        return ResponseEntity.ok(token);
+    }
+
+    @GetMapping("/validar")
+    public ResponseEntity<Usuario> validarToken(@RequestParam String token) {
+        Usuario usuario = usuarioService.validarToken(token);
+        return ResponseEntity.ok(usuario);
+    }
+
+
 }
--- src/main/java/com/aps4/APS4/autenticacao/UsuarioService.java
+++ src/main/java/com/aps4/APS4/autenticacao/UsuarioService.java
@@ -1,4 +1,65 @@
 package com.aps4.APS4.autenticacao;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.UUID;
+
 public class UsuarioService {
+
+    private final HashMap<String, Usuario> usuariosDB = new HashMap<>();
+    private final HashMap<String, Usuario> tokensDB = new HashMap<>();
+
+
+    public Usuario cadastrarUsuario(Usuario novoUsuario) {
+        String password = novoUsuario.getPassword();
+        String hashedPassword = BCrypt.hashpw(password, BCrypt.gensalt());
+        novoUsuario.setPassword(hashedPassword);
+        usuariosDB.put(novoUsuario.getEmail(), novoUsuario);
+        return novoUsuario;
+    }
+
+
+    public Collection<Usuario> listarUsuarios() {
+        return usuariosDB.values();
+    };
+
+
+    public Usuario buscarUsuario(String email) {
+        return usuariosDB.get(email);
+    }
+
+
+    public String login(Usuario usuario) {
+        Usuario userDB = usuariosDB.get(usuario.getEmail());
+        if (userDB == null) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Usuário não encontrado");
+        }
+        if (!BCrypt.checkpw(usuario.getPassword(), userDB.getPassword())) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Senha inválida");
+        }
+
+        return gerarToken(userDB);
+    }
+
+
+    public String gerarToken(Usuario usuario) {
+        String token = UUID.randomUUID().toString();
+        tokensDB.put(token, usuario);
+        return token;
+    }
+
+
+    public Usuario validarToken(String token) {
+        Usuario usuario = tokensDB.get(token);
+
+        if (usuario == null) {
+            throw new RuntimeException("Token Inválido");
+        }
+        return usuario;
+    }
+
 }
