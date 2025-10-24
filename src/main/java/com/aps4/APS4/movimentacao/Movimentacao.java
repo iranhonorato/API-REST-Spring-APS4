2693a3888b68790d415b9bde89f687aba1c0dce9
iran <iranilsonhonorato88@gmail.com>
2025-10-24 17:02:10 -0300

    feat: implementanto classe Movimentacao

--- src/main/java/com/aps4/APS4/movimentacao/Movimentacao.java
+++ src/main/java/com/aps4/APS4/movimentacao/Movimentacao.java
@@ -1,4 +1,22 @@
 package com.aps4.APS4.movimentacao;
 
+import java.time.LocalDate;
+
+
 public class Movimentacao {
+    public enum TipoMovimentacao {SAQUE, DEPOSITO}
+    private Float valor;
+    private LocalDate data;
+    private TipoMovimentacao tipo;
+
+    public Movimentacao(Float valor, LocalDate data, TipoMovimentacao tipo) {
+        this.valor = valor;
+        this.data = data;
+        this.tipo = tipo;
+    }
+
+    // Métodos get
+    public Float getValor() {return valor;}
+    public LocalDate getData() {return data;}
+    public TipoMovimentacao getTipo() {return tipo;}
 }
