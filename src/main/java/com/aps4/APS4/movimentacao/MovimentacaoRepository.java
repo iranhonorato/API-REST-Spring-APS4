7081532c1383942490749604bf9937cabc389374
iran <iranilsonhonorato88@gmail.com>
2025-10-25 13:56:39 -0300

    fix: corrigindo erro em MovimentacaoRepository

--- src/main/java/com/aps4/APS4/movimentacao/MovimentacaoRepository.java
+++ src/main/java/com/aps4/APS4/movimentacao/MovimentacaoRepository.java
@@ -1,8 +1,10 @@
 package com.aps4.APS4.movimentacao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 
+@Repository
+public interface MovimentacaoRepository extends JpaRepository<Movimentacao, Long> {
 
-public class MovimentacaoRepository extends JpaRepository<Movimentacao, Integer> {
 }
